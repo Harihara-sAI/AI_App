8d050f1310a126791f24bf6b53cf100d8f43b20e
Harihara-sAI <hahasamo1008@gmail.com>
2024-03-24 11:17:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain_community.callbacks import get_openai_callback
 import os
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
  
 # Sidebar contents
 with st.sidebar:
@@ -34,7 +34,6 @@ def main():
     # upload a PDF file
     pdf_docs = st.file_uploader("Upload your PDF",type='pdf',accept_multiple_files=True)
  
-    # st.write(pdf)
     if pdf_docs:
         text=get_pdf_text(pdf_docs)
         text_splitter = RecursiveCharacterTextSplitter(
@@ -43,28 +42,12 @@ def main():
             length_function=len
             )
         chunks = text_splitter.split_text(text=text)
- 
-        # # embeddings
-        #store_name = pdf.name[:-4]
-        #st.write(f'{store_name}')
-        # st.write(chunks)
- 
-        #if os.path.exists(f"{store_name}.pkl"):
-        #    with open(f"{store_name}.pkl", "rb") as f:
-        #        VectorStore = pickle.load(f)
-            # st.write('Embeddings Loaded from the Disk')s
-        #else:
-        #    embeddings = OpenAIEmbeddings()
-        #    VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-        #    with open(f"{store_name}.pkl", "wb") as f:
-        #        pickle.dump(VectorStore, f)
- 
+
         embeddings = OpenAIEmbeddings()
         VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
  
         # Accept user questions/query
         query = st.text_input("Ask questions here:")
-        # st.write(query)
  
         if query:
             docs = VectorStore.similarity_search(query=query, k=3)
